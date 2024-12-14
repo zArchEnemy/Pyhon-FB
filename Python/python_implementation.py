bec56e419e7f7fb1f584192509c3161acb316629
Ruslan Senatorov <senatorovmotivation@gmail.com>
2024-12-14 15:07:55 +0300

    Revert "Merge branch 'VIVALDI142857' into main"
    This reverts commit 74ecba5fc061f6f2c3e3d7de1e5548a6695a7a4c, reversing
    changes made to 5784236136e364f573c7e43234c1b3f64ce3b96e.

--- Python/python_implementation.py
+++
@@ -1,9 +0,0 @@
-"""Python Implementation of progression."""
-
-# ![telegram-cloud-photo-size-2-5433778407566599171-x.jpg](attachment:telegram-cloud-photo-size-2-5433778407566599171-x.jpg)
-
-item: list[int] = list(range(1, 6))
-summ = 0
-for num in item:
-    summ += 2 * num
-summ
